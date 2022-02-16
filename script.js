16b3e41149b1bc295a89168e8b4f18a4f4a78291
Devin Beck <devin6beck@gmail.com>
2022-02-15 18:26:28 -0700

    starting to have some function

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-const btnAddBook = document.querySelector("#btnAddBook")
-const booksDisplay = document.querySelector("#booksDisplay")
-
-btnAddBook.addEventListener("click", showLibrary)
-
-
-const booksRead = document.querySelector("#books-read");
-
-
 let myLibrary = [];
 
 function book(title, author, pages, read) {
@@ -24,11 +15,42 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+// Show everything that has been put in the library
 function showLibrary() {
+  booksDisplay.textContent = "";
   myLibrary.forEach (book => booksDisplay.textContent += `${book}, `);
 }
 
-const theHobbit = new book("The Hobbit", "J.R.R Tolkien", 295, false);
-addBookToLibrary("The Hobbit")
-addBookToLibrary("Holes")
 
+
+const btnAddBook = document.querySelector("#btnAddBook")
+const booksDisplay = document.querySelector("#booksDisplay")
+const booksRead = document.querySelector("#books-read");
+
+
+
+btnAddBook.addEventListener("click", () => {
+  // Check to see what radio button is checked. Yes = True. No = False
+  const boolRead = document.querySelector(`input[name="boolRead"]:checked`).value;
+  console.log(boolRead)
+
+  // get data out of forms
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").textContent;
+  const pages = document.querySelector("#pages").textContent;
+
+  console.log(`title: ${title}`)
+
+  // make new book from the data
+  const newBook = new book(title, author, pages, boolRead);
+
+  //add new book to library
+  addBookToLibrary(newBook);
+
+  showLibrary();
+})
+
+
+// const theHobbit = new book("The Hobbit", "J.R.R Tolkien", 295, false);
+// addBookToLibrary("The Hobbit")
+// addBookToLibrary("Holes")
